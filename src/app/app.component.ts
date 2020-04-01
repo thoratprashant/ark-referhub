adfe3cc58232d15730fb694eb8a3d2d8fa2aca19
Your Namethoratprashant <prashant.thorat@arkenea.com>
2020-04-01 18:08:25 +0530

    Added back-to-top and fix header functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'refer-hub-ark';
+
+  ngOnInit(){
+    $(window).scroll(function () {
+      var sc = $(window).scrollTop()
+    if (sc > 0) {
+        $(".rf--header").addClass("scrolled");
+        $('.back--to--top').addClass('show');
+    } else {
+        $(".rf--header").removeClass("scrolled");
+        $('.back--to--top').removeClass('show');
+    }
+  })
+
+  $(document).ready(function(){
+    $('.back--to--top').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+  });
+  }
 }
