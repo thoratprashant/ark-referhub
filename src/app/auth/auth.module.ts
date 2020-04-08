9333cdcfd2c7a99cdab5abeaeb0b1750e4a2ea73
Your Namethoratprashant <prashant.thorat@arkenea.com>
2020-04-08 10:43:28 +0530

    Added footer > ath module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthHeaderComponent } from '../shared/auth-layout/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../shared/auth-layout/auth-footer/auth-footer.component';
 import { LayoutComponent } from './layout/layout.component';
 
 
+
 @NgModule({
-  declarations: [SigninComponent, AuthHeaderComponent, LayoutComponent],
+  declarations: [SigninComponent, AuthHeaderComponent, LayoutComponent, AuthFooterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
