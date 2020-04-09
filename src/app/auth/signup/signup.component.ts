d8a0ecd63f3d185d16d6b8a51b95b322c3b94bb3
Your Namethoratprashant <prashant.thorat@arkenea.com>
2020-04-09 11:36:16 +0530

    Working on step 1

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-signup',
@@ -10,6 +12,27 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(".next-step").click(function (e) {
+      var $active = $('.stepper--indicator ul li.active');
+      $active.next().addClass('active').siblings().removeClass('active');
+      nextTab($active);
+  });
+  
+   $(".prev-step").click(function (e) {
+      var $active = $('.stepper--indicator ul li.active');
+      $active.prev().addClass('active').siblings().removeClass('active')
+      prevTab($active);
+  });
+
+  function nextTab(elem) {
+    const $tabactive = $('.tab-content .tab-pane.active');
+    $tabactive.next().addClass('active').siblings().removeClass('active');
+}
+function prevTab(elem) {
+    const $tabactive = $('.tab-content .tab-pane.active');
+    $tabactive.prev().addClass('active').siblings().removeClass('active');
+}
+
   }
 
 }
