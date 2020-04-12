36d8715821f4ec2c2e2177bf59766bca97dd23b6
Your Namethoratprashant <prashant.thorat@arkenea.com>
2020-04-13 00:10:30 +0530

    UI issues

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -12,35 +12,37 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   headerNames = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
- 
+
+  signUpStep = 1;
 
   ngOnInit(): void {
     $('#officeDentist').show();
     $('#mobileDentist').hide();
     $('#alreadyListed').hide();
     $('#sameOfficeExist').hide();
-    $('#checkDentist').change(function(){
-      if(this.checked) {
+    $('#checkDentist').change(function () {
+      if (this.checked) {
         $('#officeDentist').hide();
         $('#mobileDentist').show();
-      }else{
+      } else {
         $('#officeDentist').show();
         $('#mobileDentist').hide();
       }
-  });
-  $('.add-me-btn').on('click', function(){
-    $('#alreadyListed').show();
-    $('#officeDentist').hide();
-    $('#mobileDentist').hide();
-  })
-  $('.off-cancel-btn').on('click', function(){
-    $('#sameOfficeExist').hide();
-  })
-  $("#officeName"). keyup(function(){
-    $('#sameOfficeExist').show();
-  })
+    });
+    $('.add-me-btn').on('click', function () {
+      $('#alreadyListed').show();
+      $('#officeDentist').hide();
+      $('#mobileDentist').hide();
+    })
+    $('.off-cancel-btn').on('click', function () {
+      $('#sameOfficeExist').hide();
+    })
+    $("#officeName").keyup(function () {
+      $('#sameOfficeExist').show();
+    })
 
-    $(".next-step").click(function (e) {
+    $('body').on('click',".next-step",function (e) {
+      console.log('next step function executed');
       var $active = $('.stepper--indicator ul li.active');
       $active.next().addClass('active').siblings().removeClass('active');
       nextTab($active);
@@ -48,27 +50,39 @@ export class SignupComponent implements OnInit {
       var $active1 = $('#headerNames .steps--names.activated');
       $active1.next().addClass('activated').siblings().removeClass('activated');
 
-  });
-  
-   $(".prev-step").click(function (e) {
-      var $active = $('.stepper--indicator ul li.active');
+    })
+    .on('click',".prev-step",function (e) {
+      console.log('prev step function executed');
+      var $active = $('.stepper--indicator ul li.active');      
       $active.prev().addClass('active').siblings().removeClass('active')
       prevTab($active);
 
-      
+
       var $active1 = $('#headerNames .steps--names.activated');
       $active1.prev().addClass('activated').siblings().removeClass('activated');
-  });
+    });
 
-  function nextTab(elem) {
-    const $tabactive = $('.tab-content .tab-pane.active');
-    $tabactive.next().addClass('active').siblings().removeClass('active');
-}
-function prevTab(elem) {
-    const $tabactive = $('.tab-content .tab-pane.active');
-    $tabactive.prev().addClass('active').siblings().removeClass('active');
-}
+    function nextTab(elem) {
+      const $tabactive = $('.tab-content .tab-pane.active');
+      $tabactive.next().addClass('active').siblings().removeClass('active');
+    }
+    function prevTab(elem) {
+      const $tabactive = $('.tab-content .tab-pane.active');
+      $tabactive.prev().addClass('active').siblings().removeClass('active');
+    }
 
+  }
+
+  increaseStep(){
+    if(this.signUpStep<5){
+      this.signUpStep++;
+    }
+  }
+
+  decreseStep(){
+    if(this.signUpStep>1){
+      this.signUpStep--;
+    }
   }
 
 }
